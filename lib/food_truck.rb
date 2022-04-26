b757c8d1d7133dca3081670af1d5f6b66a44a1b8
Jim Riddle <jimriddle0@gmail.com>
2022-04-26 10:37:29 -0500

    wrote method to give me potential revenue based on food truck

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -14,4 +14,10 @@ class FoodTruck
   def stock(item, qty)
     @inventory[item] += qty
   end
+
+  def potential_revenue
+    @inventory.sum do |item|
+      item[0].price * item[1]
+    end
+  end
 end
