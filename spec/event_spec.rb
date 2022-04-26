3713b13da9f4a8576725fcc3914656dae7a82033
Jim Riddle <jimriddle0@gmail.com>
2022-04-26 11:54:16 -0500

    wrote test for selling items

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -162,7 +162,7 @@ describe Event do
                   item4 => {quantity: 50, food_trucks: [food_truck2]}
                 }
 
-      # require 'pry'; bisnding.pry
+      require 'pry'; binding.pry
 
       expect(event.total_inventory).to eq(expected)
     end
@@ -189,6 +189,39 @@ describe Event do
       event.add_food_truck(food_truck3)
 
       expect(event.overstocked_items).to eq([item1])
+    end
+
+    it 'can sell items' do
+
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+      item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      food_truck3.stock(item1, 65)
+
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.sell(item1, 200)).to eq(false)
+      expect(event.sell(item5, 1)).to eq(false)
+      expect(event.sell(item4, 5)).to eq(true)
+
+      expect(food_truck2.check_stock(item4)).to eq(45)
+
+      expect(event.sell(item1, 40)).to eq(true)
+
+      expect(food_truck1.check_stock(item1)).to eq(0)
+      expect(food_truck3.check_stock(item1)).to eq(60)
     end
 
 end
