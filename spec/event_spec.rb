fc2a2bb38a2b6e1f7dd59bb8df9fdd1b4dce511c
Jim Riddle <jimriddle0@gmail.com>
2022-04-26 10:54:28 -0500

    wrote test for total inventory

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -135,7 +135,7 @@ describe Event do
 
     end
 
-    it 'can give me overstocked items' do
+    it 'can give me total inventory' do
       event = Event.new("South Pearl Street Farmers Market")
       food_truck1 = FoodTruck.new("Rocky Mountain Pies")
       item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
@@ -156,7 +156,31 @@ describe Event do
       event.add_food_truck(food_truck2)
       event.add_food_truck(food_truck3)
 
-      expect(even.overstocked_items).to eq([item1])
+      expect(event.total_inventory).to eq({item1 => 100, item2 => 7, item3 => 35, item4 => 50})
+    end
+
+    xit 'can give me overstocked items' do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
+      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+
+      food_truck1.stock(item1, 35)
+      food_truck1.stock(item2, 7)
+      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
+      food_truck2.stock(item4, 50)
+      food_truck2.stock(item3, 25)
+      food_truck3 = FoodTruck.new("Palisade Peach Shack")
+      food_truck3.stock(item1, 65)
+      food_truck3.stock(item3, 10)
+
+      event.add_food_truck(food_truck1)
+      event.add_food_truck(food_truck2)
+      event.add_food_truck(food_truck3)
+
+      expect(event.overstocked_items).to eq([item1])
     end
 
 end
