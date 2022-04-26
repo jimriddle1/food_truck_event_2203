a632d1f589e4d8bfae06154306a4db2e2311a6cd
Jim Riddle <jimriddle0@gmail.com>
2022-04-26 12:49:32 -0500

    changed date test to mirror what was tought in class more

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -226,27 +226,9 @@ describe Event do
     end
 
     it 'can set a date' do
-      # date = Date.today
-      item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
-      item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
-      item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
-      item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
-      item5 = Item.new({name: 'Onion Pie', price: '$25.00'})
+      allow(Date).to receive(:today).and_return(Date.parse('24-02-2020'))
+
       event = Event.new("South Pearl Street Farmers Market")
-      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
-      food_truck1.stock(item1, 35)
-      food_truck1.stock(item2, 7)
-      food_truck2 = FoodTruck.new("Ba-Nom-a-Nom")
-      food_truck2.stock(item4, 50)
-      food_truck2.stock(item3, 25)
-      food_truck3 = FoodTruck.new("Palisade Peach Shack")
-      food_truck3.stock(item1, 65)
-
-      event.add_food_truck(food_truck1)
-      event.add_food_truck(food_truck2)
-      event.add_food_truck(food_truck3)
-
-      allow(event).to receive(:date).and_return("24/02/2020")
       expect(event.date).to eq("24/02/2020")
 
     end
